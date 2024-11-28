6e61b00f259fb176acf57524c4e551bee3d54f7a
Carlos Ferrer <carlosferrerhernandez10@gmail.com>
2024-11-28 00:22:40 -0500

    Syntax adjustment in model location

--- tests/Unit/LocationTest.php
+++ tests/Unit/LocationTest.php
@@ -2,9 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Models\Location;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
-use App\Models\Location;
 
 class LocationTest extends TestCase
 {
